06f159be1791d83df3007f3f84ed6f47e2ccfe7a
Gpaolo595 <giovannipaolocilento@gmail.com>
2026-05-28 09:05:32 +0200

    Aggiunti file

--- Gcyberware/public/shop.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/../components/config/app.php";
-
-require_once COMPONENTS_PATH . "/controllers/ProductController.php";
-
-$products = Product::all();
-
-include COMPONENTS_PATH . "/views/partials/navbar.php";
-include COMPONENTS_PATH . "/views/products/shop.php";
